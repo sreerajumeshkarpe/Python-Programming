f61ca860f49b6ba4cfebf06b336983893da1e76b
sreerajumeshkarpe <43986646+sreerajumeshkarpe@users.noreply.github.com>
2018-10-28 19:14:44 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,17 @@
+#name Sreeraj Karpe
+#roll no. 28
+#gr no 11810158
+
+
+def factor(x): 
+   if x==0:
+      print("1")
+   elif x==1:
+      print("1")
+   else:
+      multi=1
+      for i  in range (2,x+1,1):
+        multi=multi*i
+      print(multi)
+x=int(input("enter a number"))
+m=factor(x)
