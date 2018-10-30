d1aae1bf17c116e2b5dad85dde844310c49c4c36
sreerajumeshkarpe <43986646+sreerajumeshkarpe@users.noreply.github.com>
2018-10-31 02:22:34 +0530

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,38 @@
+import random
+from easygui import *
+ 
+words=['python','apple','rose','grapes','laptop','mouse']
+word=random.choice(words)
+
+letters = len(word)
+timesGuessed = 0
+msgbox("What word am I thinking of? You get 5 chances.")
+
+if word=='python':
+   msgbox("coding language")
+elif word=='apple':
+   msgbox("a red fruit")
+elif word=='rose':
+   msgbox("a beautiful flower")
+elif word=='grapes':
+   msgbox("nashik is famous for this fruit")
+elif word=='laptop':
+   msgbox("portable computer is called")
+else:
+   msgbox("an input device")
+while timesGuessed <= 5:
+    
+     guess = easygui.enterbox()
+     timesGuessed += 1
+
+     if guess != word:
+        msgbox("I\'m sorry, that is not correct")
+     elif guess == word:
+         msgbox("You guessed the correct word!")
+         break
+     elif timesGuessed == 5:
+         msgbox("You have run out of tries.")
+     else:
+         break
+print("The correct word was", word)
+print("Thank you for playing! Please press Enter to exit")
